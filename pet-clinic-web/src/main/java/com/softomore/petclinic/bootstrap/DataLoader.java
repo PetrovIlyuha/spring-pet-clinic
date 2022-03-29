3f9d298ef5ce9dd2b25b829d91e5f3d09832cf86
Petrov Ilya <dnamix1@gmail.com>
2022-03-29 12:33:51 +0300

    auto-generating Entity ID. Closes #23

--- pet-clinic-web/src/main/java/com/softomore/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/softomore/petclinic/bootstrap/DataLoader.java
@@ -21,13 +21,11 @@ public class DataLoader implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Jeremiah");
         owner1.setLastName("Blakemore");
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Katy");
         owner2.setLastName("Smith");
         ownerService.save(owner2);
@@ -35,14 +33,12 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("loaded 2 owners..");
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("Leonard");
         vet1.setLastName("Jackson");
 
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Iona");
         vet2.setLastName("Kirk");
 
