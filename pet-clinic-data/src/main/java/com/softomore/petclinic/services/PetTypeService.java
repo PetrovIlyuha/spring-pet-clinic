6d6cfc0ea978df01dd3320c38c5cb19ea554125c
Petrov Ilya <dnamix1@gmail.com>
2022-03-30 15:16:45 +0300

    implemented issue #29 Create PetType Map Service Impl

---
+++ pet-clinic-data/src/main/java/com/softomore/petclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package com.softomore.petclinic.services;
+
+import com.softomore.petclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
--- pet-clinic-data/src/main/java/com/softomore/petclinic/services/map/PetTypeServiceMap.java
+++ pet-clinic-data/src/main/java/com/softomore/petclinic/services/map/PetTypeServiceMap.java
@@ -1,15 +1,15 @@
 package com.softomore.petclinic.services.map;
 
-import com.softomore.petclinic.model.Pet;
-import com.softomore.petclinic.services.PetService;
+import com.softomore.petclinic.model.PetType;
+import com.softomore.petclinic.services.PetTypeService;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
 @Service
-public class PetServiceMap extends AbstractMapService<Pet, Long> implements PetService {
+public class PetTypeServiceMap extends AbstractMapService<PetType, Long> implements PetTypeService {
     @Override
-    public Set<Pet> findAll() {
+    public Set<PetType> findAll() {
         return super.findAll();
     }
 
@@ -19,17 +19,17 @@ public class PetServiceMap extends AbstractMapService<Pet, Long> implements PetS
     }
 
     @Override
-    public void delete(Pet object) {
+    public void delete(PetType object) {
         super.delete(object);
     }
 
     @Override
-    public Pet save(Pet object) {
+    public PetType save(PetType object) {
         return super.save(object);
     }
 
     @Override
-    public Pet findById(Long id) {
+    public PetType findById(Long id) {
         return super.findById(id);
     }
 }
