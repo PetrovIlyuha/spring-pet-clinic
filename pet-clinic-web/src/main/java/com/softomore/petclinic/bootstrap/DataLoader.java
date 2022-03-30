7753b57f903f2b7b0f0a5aadad03a900956e6cfe
Petrov Ilya <dnamix1@gmail.com>
2022-03-30 15:23:28 +0300

    add seed data for couple of pet types. Closes #32

--- pet-clinic-web/src/main/java/com/softomore/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/softomore/petclinic/bootstrap/DataLoader.java
@@ -1,8 +1,10 @@
 package com.softomore.petclinic.bootstrap;
 
 import com.softomore.petclinic.model.Owner;
+import com.softomore.petclinic.model.PetType;
 import com.softomore.petclinic.model.Vet;
 import com.softomore.petclinic.services.OwnerService;
+import com.softomore.petclinic.services.PetTypeService;
 import com.softomore.petclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -12,14 +14,25 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+        PetType dog = new PetType();
+        dog.setName("dog");
+        PetType savedDogPetType = petTypeService.save(dog);
+
+
+        PetType cat = new PetType();
+        cat.setName("cat");
+        PetType savedCatPetType = petTypeService.save(cat);
+
         Owner owner1 = new Owner();
         owner1.setFirstName("Jeremiah");
         owner1.setLastName("Blakemore");
