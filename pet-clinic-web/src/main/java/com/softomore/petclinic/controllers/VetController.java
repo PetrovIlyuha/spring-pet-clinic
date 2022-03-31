e4004b5cbcfd36a11544e4133e2032c319fd1469
Petrov Ilya <dnamix1@gmail.com>
2022-03-31 17:05:44 +0300

    resolved not working menu links. Closes #45

--- pet-clinic-web/src/main/java/com/softomore/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/softomore/petclinic/controllers/VetController.java
@@ -13,7 +13,7 @@ public class VetController {
         this.vetService = vetService;
     }
 
-    @RequestMapping({"/vets","/vets/index", "/vets/index.html"})
+    @RequestMapping({"/vets","/vets/index", "/vets/index.html", "/vets.html"})
     public String listVets(Model model) {
         System.out.println(vetService.findAll());
         model.addAttribute("vets", vetService.findAll());
