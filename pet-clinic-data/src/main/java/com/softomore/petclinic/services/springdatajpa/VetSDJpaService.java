84fe160a3e0c4e4e18e215a8c13578344cb8d385
Petrov Ilya <dnamix1@gmail.com>
2022-04-04 15:13:10 +0300

    Created Spring Data JPA Service for Vets. Closes #41

--- pet-clinic-data/src/main/java/com/softomore/petclinic/services/springdatajpa/VetSDJpaService.java
+++ pet-clinic-data/src/main/java/com/softomore/petclinic/services/springdatajpa/VetSDJpaService.java
@@ -1,40 +1,48 @@
 package com.softomore.petclinic.services.springdatajpa;
 
 import com.softomore.petclinic.model.Vet;
+import com.softomore.petclinic.repositories.VetRepository;
 import com.softomore.petclinic.services.VetService;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
+import java.util.HashSet;
 import java.util.Set;
 
 @Service
 @Profile("springdatajpa")
 public class VetSDJpaService implements VetService {
 
+    private final VetRepository vetRepository;
 
+    public VetSDJpaService(VetRepository vetRepository) {
+        this.vetRepository = vetRepository;
+    }
 
     @Override
     public Set<Vet> findAll() {
-        return null;
+        Set<Vet> vets = new HashSet<>();
+        vetRepository.findAll().forEach(vets::add);
+        return vets;
     }
 
     @Override
     public Vet findById(Long aLong) {
-        return null;
+        return vetRepository.findById(aLong).orElse(null);
     }
 
     @Override
     public Vet save(Vet object) {
-        return null;
+        return vetRepository.save(object);
     }
 
     @Override
     public void delete(Vet object) {
-
+        vetRepository.delete(object);
     }
 
     @Override
     public void deleteById(Long aLong) {
-
+        vetRepository.deleteById(aLong);
     }
 }
